5551579060c854265f45b946d22a19e76aa276b8
Micheleen <michhar@microsoft.com>
2017-04-19 18:33:50 -0700

    Update __init__.py

--- msbot/__init__.py
+++ msbot/__init__.py
@@ -16,8 +16,9 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-cwd = os.getcwd()
-auth_config = os.path.join(cwd, 'auth_config.json')
+# cwd = os.getcwd()
+# auth_config = os.path.join(cwd, 'auth_config.json')
+auth_config = 'https://gist.githubusercontent.com/michhar/d51d5fa5d4a3ec8f9b73e9d47db906b4/raw/bbcafd3cd4b8da4bed19ceb0b8511cbc7da8e1bb/auth_config.json'
 
 config = {'OIDC_CLIENT_SECRETS': auth_config,
 'OIDC_SCOPES': ["https://api.botframework.com/.default"],
