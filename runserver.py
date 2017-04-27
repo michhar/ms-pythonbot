f2e3adff36a5c1d5e713cef4983d429ba740b038
michhar <michhar@microsoft.com>
2017-04-27 08:30:48 -0700

    updated path to auth file for download

--- runserver.py
+++ runserver.py
@@ -29,18 +29,18 @@ if __name__ == '__main__':
 
     # Test system platform to decide which way to start celery and redis
 
-    # # Win
-    # if _platform.startswith("win"):
-    #     # Start redis server as broker for celery processes first
-    #     os.system('start \B rediswin\\redis-server.exe rediswin\\redis.windows.conf')
-    #     # Start celery for asynchronous task queues
-    #     os.system('start \B celery -A msbot.views.celery_app worker -l info')
-    # # Linus or Mac OSX
-    # else: #_platform.startswith("darwin") or _platform.startswith("linux" ):
-    #     # Start redis server as broker for celery processes first
-    #     os.system('../redisunix/src/redis-server &')
-    #     # Start celery for asynchronous task queues
-    #     os.system('celery -A msbot.views.celery_app worker -l info &')
+    # Win
+    if _platform.startswith("win"):
+        # Start redis server as broker for celery processes first
+        os.system('start rediswin\\redis-server.exe rediswin\\redis.windows.conf')
+        # Start celery for asynchronous task queues
+        os.system('start celery -A msbot.views.celery_app worker -l info')
+    # Linus or Mac OSX
+    else: #_platform.startswith("darwin") or _platform.startswith("linux" ):
+        # Start redis server as broker for celery processes first
+        os.system('../redisunix/src/redis-server &')
+        # Start celery for asynchronous task queues
+        os.system('celery -A msbot.views.celery_app worker -l info &')
 
 
     # Run flask app on port specified here
