178b62b842093b2f6dbbb0bf5ac4a3ddfd8a71a0
michhar <michhar@microsoft.com>
2017-06-19 23:12:55 -0700

    updated some app settings

--- msbot/__init__.py
+++ msbot/__init__.py
@@ -18,7 +18,7 @@ app.config['DEBUG'] = True
 
 cwd = os.getcwd()
 # auth_config = os.path.join(cwd, 'auth_config.json')
-url = 'https://gist.githubusercontent.com/michhar/d51d5fa5d4a3ec8f9b73e9d47db906b4/raw/bbcafd3cd4b8da4bed19ceb0b8511cbc7da8e1bb/auth_config.json'
+url = 'https://gist.githubusercontent.com/michhar/d51d5fa5d4a3ec8f9b73e9d47db906b4/raw/2faf799dda51087ff75ebe861933b305b42501f6/auth_config.json'
 file_name = os.path.join(cwd, 'msbot', 'auth_config.json')
 # Download the file from `url` and save it locally under `file_name`:
 with urllib.request.urlopen(url) as response, open(file_name, 'wb') as out_file:
