62134bcc0912cbf2818285b5732bcc427d646fa3
michhar <michhar@microsoft.com>
2017-09-04 10:28:02 -0700

    read/write auth file

--- msbot/__init__.py
+++ msbot/__init__.py
@@ -11,6 +11,7 @@ from flask import Flask
 from flask_oidc import OpenIDConnect
 from .callback_utils import Callbacks
 import os
+import json
 
 
 app = Flask(__name__)
@@ -18,6 +19,16 @@ app.config['DEBUG'] = True
 
 cwd = os.getcwd()
 auth_config = os.path.join(cwd, 'auth_config.json')
+
+
+with open(auth_config, 'w+') as data_file:
+    data = json.load(data_file)
+    data['client_id'] = os.getenv('CLIENT_ID', 'foo')
+    data['client_secret'] = os.getenv('CLIENT_SECRET', 'bar')
+    print(data)
+    json.dumps(data, data_file)
+# auth_config = os.path.join(cwd + '/msbot/', 'auth_config_true.json')
+
 
 config = {'OIDC_CLIENT_SECRETS': auth_config,
           'OIDC_SCOPES': ["https://api.botframework.com/.default"],
