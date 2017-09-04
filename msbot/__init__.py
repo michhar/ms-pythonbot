70720dac831146d3e48c7a487404e13a374ea493
michhar <michhar@microsoft.com>
2017-09-04 15:59:15 -0700

    fixing because win uses relative paths

--- msbot/__init__.py
+++ msbot/__init__.py
@@ -12,31 +12,37 @@ from flask_oidc import OpenIDConnect
 from .callback_utils import Callbacks
 import os
 import json
+from sys import platform as _platform
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 cwd = os.getcwd()
-auth_config_tmpl = os.path.join(cwd, 'msbot', 'auth_config_template.json')
-auth_config = os.path.join('..', cwd, 'auth_config.json')
+
+# Windows uses relative paths
+if _platform.startswith("win"):
+    auth_config_tmpl = os.path.join('auth_config_template.json')
+    auth_config = os.path.join('..', 'auth_config.json')
+else:
+    auth_config_tmpl = os.path.join(cwd, 'msbot', 'auth_config_template.json')
+    auth_config = os.path.join(cwd, 'auth_config.json')
 
 # For unix compatibility
-auth_config = os.path.join(cwd, 'auth_config.json')
 
 
 with open(auth_config_tmpl, 'r') as data_file:
     data = json.load(data_file)
     data['web']['client_id'] = os.getenv('MICROSOFT_CLIENT_ID', 'foo')
     data['web']['client_secret'] = os.getenv('MICROSOFT_CLIENT_SECRET', 'bar')
-    print(data)
     with open(auth_config, 'w') as auth:
         json.dump(data, auth)
 
 
 config = {'OIDC_CLIENT_SECRETS': auth_config,
           'OIDC_SCOPES': ["https://api.botframework.com/.default"],
-          'OIDC_RESOURCE_SERVER_ONLY': True}
+          'OIDC_RESOURCE_SERVER_ONLY': True,
+          }
 
 app.config.update(config)
 oidc = OpenIDConnect(app)
--- runserver.py
+++ runserver.py
@@ -9,7 +9,7 @@ from msbot import app
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
+        PORT = int(environ.get('SERVER_PORT', '3978'))
     except ValueError:
-        PORT = 5555
+        PORT = 3978
     app.run(HOST, PORT)
