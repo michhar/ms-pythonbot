65205e0d603ac2f7755050ef09ba6f9c6f666c60
michhar <michhar@microsoft.com>
2017-09-18 23:24:46 -0700

    update auth uri and init

--- msbot/__init__.py
+++ msbot/__init__.py
@@ -19,7 +19,8 @@ app = Flask(__name__)
 app.config['DEBUG'] = True
 
 cwd = os.getcwd()
-
+auth_config_tmpl = ''
+auth_config = ''
 # Windows uses relative paths
 if _platform.startswith("win"):
     auth_config_tmpl = os.path.join('auth_config_template.json')
@@ -30,13 +31,13 @@ else:
 
 # For unix compatibility
 
-
+data = {}
 with open(auth_config_tmpl, 'r') as data_file:
     data = json.load(data_file)
     data['web']['client_id'] = os.getenv('MICROSOFT_CLIENT_ID', 'foo')
     data['web']['client_secret'] = os.getenv('MICROSOFT_CLIENT_SECRET', 'bar')
-    with open(auth_config, 'w') as auth:
-        json.dump(data, auth)
+with open(auth_config, 'w') as auth:
+    json.dump(data, auth)
 
 
 config = {'OIDC_CLIENT_SECRETS': auth_config,
